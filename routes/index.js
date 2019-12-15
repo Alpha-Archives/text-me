44c23e5f5e018bea125bdc9f437278101ff72205
Alpha Olomi <alphaolomi@gmail.com>
2019-12-15 11:29:33 +0300

    add post route for sms's

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const Nexmo = require("nexmo");
+
+const nexmo = new Nexmo(
+  {
+    apiKey: "YOURAPIKEY",
+    apiSecret: "YOURAPISECRET"
+  },
+  { debug: true }
+);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "TextMe" });
+});
+
+/* POST home page*/
+
+router.post("/", (req, res) => {
+  const { number, text } = req.body;
+
+  nexmo.message.sendSms("YOURVURTUALNUMBER", number, text, { type: "unicode" }, (err, responseData) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const { messages } = responseData;
+      const { ["message-id"]: id, ["to"]: number, ["error-text"]: error } = messages[0];
+      console.dir(responseData);
+      // Get data from response
+      const data = {
+        id,
+        number,
+        error
+      };
+
+      // Emit to the client
+      io.emit("smsStatus", data);
+    }
+  });
 });
 
 module.exports = router;
