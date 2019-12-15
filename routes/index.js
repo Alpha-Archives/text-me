6c03b221a84b6f50ad5551e7195f0b91dd7f9fae
Alpha Olomi <alphaolomi@gmail.com>
2019-12-15 11:40:01 +0300

    wip feat: complete

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const Nexmo = require("nexmo");
 
 const nexmo = new Nexmo(
   {
-    apiKey: "YOURAPIKEY",
-    apiSecret: "YOURAPISECRET"
+    apiKey: process.env.API_KEY || "YOURAPIKEY",
+    apiSecret: process.env.API_SECRET || "YOURAPISECRET"
   },
-  { debug: true }
+  { debug: process.env.DEBUGING || true }
 );
+
+const VIRTUAL_NUMBER = process.env.API_SECRET || "YOURVURTUALNUMBER";
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -20,14 +22,13 @@ router.get("/", function(req, res, next) {
 router.post("/", (req, res) => {
   const { number, text } = req.body;
 
-  nexmo.message.sendSms("YOURVURTUALNUMBER", number, text, { type: "unicode" }, (err, responseData) => {
+  nexmo.message.sendSms(VIRTUAL_NUMBER, number, text, { type: "unicode" }, (err, responseData) => {
     if (err) {
       console.log(err);
     } else {
       const { messages } = responseData;
       const { ["message-id"]: id, ["to"]: number, ["error-text"]: error } = messages[0];
       console.dir(responseData);
-      // Get data from response
       const data = {
         id,
         number,
