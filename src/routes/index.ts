0d3bc0a1164dcbe65ce99c9b1cb972c3fb277daa
Alpha Olomi <alphaolomi@gmail.com>
2021-02-22 19:35:21 +0300

    wip:

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,20 @@
 import * as express from "express";
 const router = express.Router();
 
-/* GET home page. */
-router.get(
-  "/",
-  (req: express.Request, res: express.Response, next: express.Next) => {
-    res.render("index", { title: "Express" });
-  }
+import Nexmo from "nexmo";
+
+// Init Nexmo
+const nexmo = new Nexmo(
+  {
+    apiKey: process.env.NEXMO_API_KEY || "YOURAPIKEY",
+    apiSecret: process.env.NEXMO_API_SECRET || "YOURAPISECRET",
+  },
+  { debug: true }
 );
+
+/* GET home page. */
+router.get("/", (req: express.Request, res: express.Response) => {
+  res.render("index", { title: "Express" });
+});
 
 export default router;
